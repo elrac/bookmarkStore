897b8fd9455967faeb4b2c17548ade8a7f5dfb56
elrac <elrac1@gmail.com>
2015-09-28 08:02:41 -0700

    added endpoint for root

--- src/bookmarkResource.js
+++ src/bookmarkResource.js
@@ -1,11 +1,14 @@
 exports.setup = function(server, db){
 
-
-  server.get('/bookmarks',function(req,res,next){
+  var getBookmarks = function(req,res,next){
     db.all('select * from bookmark',function(err, data){
       console.log(data);
       res.send(data);
       next();
     });
-  });
+  }
+
+
+  server.get('/bookmark',getBookmarks);
+  server.get('/',getBookmarks);
 }
