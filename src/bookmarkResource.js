c53f4977a469fc382087b5e02837146f47b9fe6d
elrac <elrac1@gmail.com>
2015-09-28 07:54:38 -0700

    made /bookmarks actually send data

--- src/bookmarkResource.js
+++ src/bookmarkResource.js
@@ -2,8 +2,10 @@ exports.setup = function(server, db){
 
 
   server.get('/bookmarks',function(req,res,next){
-    db.all('select * from bookmark',function(err, res){
-      console.log(res);
+    db.all('select * from bookmark',function(err, data){
+      console.log(data);
+      res.send(data);
+      next();
     });
   });
 }
